0762ee33b76f2f10ff315de2338358adeb005d7a
Alana Li <ruiruialana@gmail.com>
2024-02-10 03:36:00 -0800

    Removing all broken js

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 const vform = document.getElementById('valentineForm');
 const ocolor = document.body.style.backgroundColor;
-if (vform === null) {
-    document.body.style.backgroundColor = 'red';
-}
 
 vform.addEventListener('submit', function(event) {
     const yourName = document.getElementById('yourName');
     const valentineName = document.getElementById('valentineName');
-
     if (!yourName.value || !valentineName.value) {
-        const message = document.createElement('p');
-        message.textContent = 'Please fill out both names.';
-        message.classList.add('message');
         document.body.style.backgroundColor = 'red';
-        document.body.appendChild(message);
-        const submitButton = document.querySelector('#go-button button');
-        submitButton.parentNode.insertBefore(message, submitButton.nextSibling);
         setTimeout(function() {
-            document.body.removeChild(message);
             document.body.style.backgroundColor = ocolor;
         }, 2000);
         event.preventDefault();
-        highlightEmptyInputs(yourName, valentineName);
     }
 });
