e2ed1efbbced43597f223eee8ca322b66b13087f
Alana Li <ruiruialana@gmail.com>
2024-02-10 00:03:54 -0800

    Javascript debugging

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const form = document.getElementById('valentineForm');
-
-form.addEventListener('submit', function(event) {
-    const yourName = document.getElementById('yourName');
-    const valentineName = document.getElementById('valentineName');
-
-    if (!yourName.value || !valentineName.value) {
-        event.preventDefault();
-        if (!yourName.value) {
-            yourName.setCustomValidity('Please enter your name');
-        } else {
-            yourName.setCustomValidity('');
-        }
-        if (!valentineName.value) {
-            valentineName.setCustomValidity('Please enter your valentine\'s name');
-        } else {
-            valentineName.setCustomValidity('');
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    const vform = document.getElementById('valentineForm');
+    if (vform === null) {
+        document.body.style.backgroundColor = 'red';
     }
+
+    vform.addEventListener('submit', function(event) {
+        document.body.style.backgroundColor = 'green';
+        const yourName = document.getElementById('yourName');
+        const valentineName = document.getElementById('valentineName');
+
+        if (!yourName.value || !valentineName.value) {
+            event.preventDefault();
+            if (!yourName.value) {
+                yourName.setCustomValidity('Please enter your name');
+            } else {
+                yourName.setCustomValidity('');
+            }
+            if (!valentineName.value) {
+                valentineName.setCustomValidity('Please enter your valentine\'s name');
+            } else {
+                valentineName.setCustomValidity('');
+            }
+        }
+    });
 });
