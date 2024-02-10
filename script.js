bd73f327ea0360ce0b6b64eecd6f497094b113b0
Alana Li <ruiruialana@gmail.com>
2024-02-10 03:55:59 -0800

    Smooth red animation

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const vform = document.getElementById('valentineForm');
-const ocolor = document.body.style.backgroundColor;
+document.addEventListener('DOMContentLoaded', function() {
+    const vform = document.getElementById('valentineForm');
 
-vform.addEventListener('submit', function(event) {
-    const yourName = document.getElementById('yourName');
-    const valentineName = document.getElementById('valentineName');
-    if (!yourName.value || !valentineName.value) {
-        document.body.style.backgroundColor = 'red';
-        setTimeout(function() {
-            document.body.style.backgroundColor = ocolor;
-        }, 2000);
-        event.preventDefault();
-    }
+    vform.addEventListener('submit', function(event) {
+        const yourName = document.getElementById('yourName');
+        const valentineName = document.getElementById('valentineName');
+        if (!yourName.value || !valentineName.value) {
+            document.body.classList.add('transition');
+            document.body.style.backgroundColor = 'red';
+            setTimeout(function() {
+                document.body.style.backgroundColor = 'white';
+                setTimeout(function() {
+                    document.body.classList.remove('transition');
+                }, 1000);
+            }, 1000);
+            event.preventDefault();
+        }
+    });
 });
